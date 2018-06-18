143e482fbbe48cd423307214c31ba70a801df176
dehahost <tot98@email.com>
2018-06-19 00:32:48 +0200

    Uploaded project data
    C# code! :-)

---
+++ project/SonarWiper/Program.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * SonarWiper
+ * ============
+ * 
+ * Wipe whole folder with files that each contains usage of Adobe's products.
+ * 
+ * Written in SharpDevelop by dehahost.
+ * Release version is 1.2
+ */
+using System;
+using System.IO;
+using System.Security.Permissions;
+using System.Timers;
+
+namespace Sonar_Wiper {
+	class Program {
+		static Timer waiter;
+		
+		public static void Main(string[] args) {			
+			if ( args.Length == 0 ) {
+				FolderCheck();				
+			} else if( args.Length == 1 ) {
+				if ( args[0].ToLower() == "-stay" || args[0].ToLower() == "/stay" ) {
+					FolderCheck();
+					WatchSonar();
+				} else
+					WriteOutHelp();
+			} else
+				WriteOutHelp();
+			
+			Console.WriteLine( "\r\n[{0}] [GOOD/BYE] Exitting...", DateTime.Now.ToString() );
+		}
+		
+		[PermissionSet(SecurityAction.Demand, Name="FullTrust")]
+		private static void WatchSonar() {
+			Console.Write( "[{0}] [WATCH/INFO] Setting up folder watcher...", DateTime.Now.ToString() );
+			try {
+				FileSystemWatcher watcher = new FileSystemWatcher();
+				// Watch %AppData%\Adobe for changes
+				watcher.Path = Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) + "\\Adobe";
+				// Fileter events
+				watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.DirectoryName;
+				// Watch also subdirs
+				watcher.IncludeSubdirectories = true;
+				// Watch only for created and deleted things
+				watcher.Created += watcher_Created;
+				watcher.Deleted += watcher_Deleted;
+				watcher.Changed += watcher_Changed;
+				// Wake up, open your eyes and start watching
+				watcher.EnableRaisingEvents = true;
+				Console.WriteLine("  OK!");
+				
+				// Wait for the user to quit the program.
+		        Console.WriteLine("Press [Q] then [Enter] to quit.");
+		        while( Console.Read() != 'q' ) {}
+			} catch (ArgumentException exar) {
+				Console.WriteLine( "  FAILED!" );
+				Console.WriteLine( "[{0}] [FOLDER_CHECK/MISSING] %AppData%\\Adobe is not present.", DateTime.Now.ToString() );
+			}
+		}
+
+		static void watcher_Created(object sender, FileSystemEventArgs e) {
+			if ( e.Name.StartsWith( "Sonar" ) ) {
+				Console.WriteLine("[{0}] [WATCH/CREATED] Sometghing called \"{1}\" was just created.", DateTime.Now.ToString(), e.Name );
+				HandleTheWaiter();
+		    }
+		}
+
+		static void watcher_Changed(object sender, FileSystemEventArgs e) {
+			if ( e.Name.StartsWith( "Sonar" ) ) {
+				Console.WriteLine("[{0}] [WATCH/MODIFY] Something called \"{1}\" was just modified.", DateTime.Now.ToString(), e.Name );
+				HandleTheWaiter();
+		    }
+		}
+		
+		static void watcher_Deleted(object sender, FileSystemEventArgs e) {
+			if ( e.Name.StartsWith( "Sonar" ) )
+				Console.WriteLine( "[{0}] [WATCH/DELETE] Something called \"{1}\" was just removed forever.", DateTime.Now.ToString(), e.Name );
+		}
+
+		static void waiter_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			// Just delete Sonar folder
+			Console.WriteLine( "[{0}] [WATCH/INFO] Deleting of Sonar folder in progress...", DateTime.Now.ToString() );
+			Directory.Delete( Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) + "\\Adobe\\Sonar\\", true );
+			
+			waiter.Close();
+		}
+		
+		private static bool FolderCheck() {
+			Console.WriteLine( "[{0}] [FOLDER_CHECK/INFO] Checking for Adobe's Sonar folder in %AppData%\\Adobe\\...", DateTime.Now.ToString() );
+			if ( Directory.Exists( Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) + "\\Adobe\\Sonar\\" ) ) {
+				Console.Write("[{0}] [FOLDER_CHECK/INFO] Removing existing Sonar folder...", DateTime.Now.ToString() );
+				Directory.Delete( Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) + "\\Adobe\\Sonar\\", true );
+				Console.WriteLine( "  OK!" );
+				return true;
+			}
+			Console.WriteLine( "[{0}] [FOLDER_CHECK/MISSING] Sonar folder is not present.", DateTime.Now.ToString() );
+			return false;
+		}
+		
+		private static void HandleTheWaiter() {
+			if ( waiter == null ) {
+				waiter = new Timer( 50 );
+				waiter.AutoReset = false;
+				waiter.Elapsed += waiter_Elapsed;
+			}
+			
+			if ( waiter.Enabled)
+				waiter.Stop();
+			waiter.Start();
+		}
+		
+		private static void WriteOutHelp() {
+			Console.WriteLine(
+				"Sonar Wiper v1.2 by dehahost\r\n" +
+				"======================\r\n" +
+				"Simple wiper to hide out your usage of Adobe products.\r\n" +
+				"\r\n" +
+				"Command usage:\r\n" +
+				"  sonarwiper.exe [/stay]\r\n"+
+				"\r\n" +
+				"Arguments:\r\n" +
+				"  /stay   Sonar Wiper will stay running and automatically delete Sonar folder after changes was made."
+			);
+		}
+	}
+}
---
+++ project/SonarWiper/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("SonarWiper")]
+[assembly: AssemblyDescription ("Opt-out Adobe's product usage statistics.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("dehahost")]
+[assembly: AssemblyProduct ("sonarwiper")]
+[assembly: AssemblyCopyright ("dehahost 2018")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.2.*")]
+[assembly: AssemblyFileVersion ("1.2")]
